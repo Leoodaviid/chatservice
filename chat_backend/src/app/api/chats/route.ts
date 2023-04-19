dffcb1e331ae9c06c08e6f99d97355e32f871429
Leoodaviid <david_ax7@hotmail.com>
2023-04-19 20:40:38 -0300

    feat: endpoint integration chat

--- chat_backend/src/app/api/chats/route.ts
+++ chat_backend/src/app/api/chats/route.ts
@@ -1,22 +1,39 @@
-import {prisma} from "../../prisma/prisma"
+import { prisma } from "../../prisma/prisma"
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
+    const body = await request.json()
+    const chatCreated = await prisma.chat.create({
+        data: {
+            messages: {
+                create: {
+                    content: body.message,
+                },
+            },
+        },
+        select: {
+            id: true,
+            messages: true
+        },
+    });
 
-  const chatCreated = await prisma.chat.create({
-    data: {},
-  });
 
-    
-    return  NextResponse.json(chatCreated);
+    return NextResponse.json(chatCreated);
 }
 
 export async function GET(request: NextRequest) {
-    
+
     const chats = await prisma.chat.findMany({
-      orderBy:{
-        created_at: "desc",
-      }, 
+        select: {
+            id: true,
+            messages: {
+                orderBy: { created_at: "asc" },
+                take: 1,
+            },
+        },
+        orderBy: {
+            created_at: "desc",
+        },
     });
 
     return NextResponse.json(chats);
