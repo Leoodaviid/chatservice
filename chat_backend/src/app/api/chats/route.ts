9e27e1886cfe5ac7e90106160d3b7c991f955de7
Leoodaviid <david_ax7@hotmail.com>
2023-04-19 17:46:53 -0300

    feat: endpoint integration, POST start chats; GET list chats;

---
+++ chat_backend/src/app/api/chats/route.ts
@@ -0,0 +1,23 @@
+import {prisma} from "../../prisma/prisma"
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+
+  const chatCreated = await prisma.chat.create({
+    data: {},
+  });
+
+    
+    return  NextResponse.json(chatCreated);
+}
+
+export async function GET(request: NextRequest) {
+    
+    const chats = await prisma.chat.findMany({
+      orderBy:{
+        created_at: "desc",
+      }, 
+    });
+
+    return NextResponse.json(chats);
+}
